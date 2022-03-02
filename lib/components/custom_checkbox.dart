055a1b022af78f33d1b280a8752ec017c02e3ddf
faiq khan <shayankhan480@gmail.com>
2022-03-02 14:28:24 +0500

    set intermediate selection..

---
+++ lib/components/custom_checkbox.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:vyv/utils/app_colors.dart';
+
+class CustomCheckBox extends StatelessWidget {
+  final VoidCallback action;
+  final bool isSelected;
+  final IconData icon;
+  const CustomCheckBox({Key? key, required this.action, required this.isSelected, required this.icon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: AnimatedContainer(
+        duration: Duration(microseconds: 500),
+        curve: Curves.fastLinearToSlowEaseIn,
+        decoration: BoxDecoration(
+          color: isSelected ? AppColors.primaryColor : Colors.transparent,
+          borderRadius: BorderRadius.circular(2.0),
+          border: isSelected ? null : Border.all(color: Colors.black54, width: 2.0)
+        ),
+        height: 25,
+        width: 25,
+        child: isSelected ? Icon(icon, color: AppColors.white,) : null,
+      ),
+    );
+  }
+}
--- lib/components/search_sheet.dart
+++ lib/components/search_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vyv/controllers/search_controller.dart';
@@ -6,6 +7,8 @@ import 'package:vyv/models/district.dart';
 import 'package:vyv/models/search_model.dart';
 import 'package:vyv/utils/app_colors.dart';
 import 'package:vyv/widgets/text_component.dart';
+
+import 'custom_checkbox.dart';
 
 class SearchBottomSheet extends GetView<SearchController> {
 // class SearchBottomSheet extends StatelessWidget {
@@ -31,23 +34,30 @@ class SearchBottomSheet extends GetView<SearchController> {
   Widget districtTile(index) {
     District _district = controller.districts.elementAt(index);
     List<County> _counties = controller.counties.where((c) => c.idDistrict == _district.id).toList();
-    return Obx(
-        () => ExpansionTile(
+    return Obx(() {
+      bool _value = controller.selected.any((element) => element.district.id == _district.id);
+      return ExpansionTile(
         // title: TextWidget(text: controller.districts.elementAt(index).districtName),
-        title: CheckboxListTile(
-          title: TextWidget(text: _district.districtName),
-          value: controller.selected.any((element) => element.district.id == _district.id),
-          onChanged: (value) => controller.handleDistrict(value!, _district, _counties),
+        title: ListTile(
+          title: TextWidget(text: _district.districtName, size: 2.0,),
+          trailing: CustomCheckBox(
+            isSelected: controller.selected.any((element) => element.district.id == _district.id),
+            action: () => controller.handleDistrict(!_value, _district, _counties),
+            icon:
+            (_value && controller.selected.firstWhere((element) => element.district.id == _district.id).counties.length != _counties.length) ?
+            CupertinoIcons.minus :  Icons.check,
+          ),
+          onTap: () => controller.handleDistrict(!_value, _district, _counties),
         ),
         children: List.generate(_counties.length, (countyIndex) => countiesTile(countyIndex, _district, _counties.elementAt(countyIndex))),
         // initiallyExpanded: true,
-      ),
-    );
+      );
+    });
   }
 
   Widget countiesTile(int index, District district,  County county) {
     return CheckboxListTile(
-      title: TextWidget(text: county.countyName),
+      title: TextWidget(text: county.countyName, size: 1.8,),
       // value: controller.check(),
       value: controller.selected.isNotEmpty ? controller.selected.firstWhere((element) => element.district.id == district.id).counties.any((c) => c.id == county.id) : false,
       onChanged: (value) => controller.handleCounty(value!, district, county),
